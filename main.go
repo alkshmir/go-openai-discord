c75a8b40e08165433d03d469810dba24830c9778
alkshmir <shirakam.itsuk252@gmail.com>
2024-04-14 12:30:07 +0900

    add openai API error handling
    fixes #1

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"regexp"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 var gpt = chatBot{}
@@ -73,7 +76,31 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	reply, err := gpt.Reply(content)
 	if err != nil {
-		log.Fatal(err)
+		e := &openai.APIError{}
+		if errors.As(err, &e) {
+			switch e.HTTPStatusCode {
+			case 400:
+				if strings.Contains(e.Message, "Please reduce the length of the messages.") {
+					// Initialize the client and clear the message history
+					gpt.Init()
+					s.ChannelMessageSend(m.ChannelID, "Cleared the message history as reached maximum token length. Please retry.")
+				}
+			case 401:
+				// invalid auth or key (do not retry)
+				log.Fatal("Invalid auth or key")
+			case 429:
+				// rate limiting or engine overload (wait and retry)
+				log.Println(err)
+				s.ChannelMessageSend(m.ChannelID, e.Message)
+			case 500:
+				// openai server error (retry)
+				log.Println(err)
+				s.ChannelMessageSend(m.ChannelID, e.Message)
+			default:
+				// unhandled
+				log.Fatal(err)
+			}
+		}
 	}
 	s.ChannelMessageSend(m.ChannelID, reply)
 }
